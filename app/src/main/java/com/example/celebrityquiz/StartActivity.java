59d643d48dd8f7d9c94f1b4ab938679b2cc8ac5e
cmkxak <gcm466@naver.com>
2021-04-06 22:21:20 +0900

    second commit

--- app/src/main/java/com/example/celebrityquiz/StartActivity.java
+++ app/src/main/java/com/example/celebrityquiz/StartActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class StartActivity extends AppCompatActivity {
@@ -20,6 +21,8 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Objects.requireNonNull(getSupportActionBar()).hide(); //액션바 숨김
 
         btn_start= (Button)findViewById(R.id.btn_start);
         btn_start.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +43,14 @@ public class StartActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btn_chkscore = (Button)findViewById(R.id.btn_chkscore);
+        btn_chkscore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
 
