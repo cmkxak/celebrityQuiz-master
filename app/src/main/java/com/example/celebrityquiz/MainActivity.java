4735674ac317155326d4f35d89c4dad95c8d24a7
cmkxak <gcm466@naver.com>
2021-04-10 20:09:09 +0900

    액티비티 이름 변경

--- app/src/main/java/com/example/celebrityquiz/MainActivity.java
+++ app/src/main/java/com/example/celebrityquiz/MainActivity.java
@@ -4,132 +4,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.RadioButton;
-import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_start;
+    Button btn_setting;
+    Button btn_chkscore; //점수 확인 버튼
 
-    // Declare Variables
-    private RadioButton radioButtonLevelOne;
-    private RadioButton radioButtonLevelTwo;
-    private RadioButton radioButtonLevelThree;
-    private RadioButton radioButton30;
-    private RadioButton radioButton60;
-    private RadioButton radioButton90;
-    private ProgressBar progressBarDownload;
-    private Button buttonStartQuiz;
-    public int level;
-    public int seconds;
-    Button btn_start, btn_stop;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_start = (Button)findViewById(R.id.btn_start);
-        btn_stop = (Button)findViewById(R.id.btn_stop);
+        Objects.requireNonNull(getSupportActionBar()).hide(); //액션바 숨김
 
-        btn_start.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startService(new Intent(getApplicationContext(), MusicService.class));
+        startService(new Intent(getApplicationContext(), MusicService.class));
+
+        btn_start= (Button)findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+                Random random = new Random();
+
+                intent.putExtra("level", random.nextInt(3));
+                intent.putExtra("seconds",random.nextInt(90));
+                startActivity(intent);
             }
         });
-        btn_stop.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                stopService(new Intent(getApplicationContext(), MusicService.class));
+
+        btn_setting = (Button) findViewById(R.id.btn_setting);
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
+                startActivity(intent);
             }
         });
 
-        // Define Level views
-        radioButtonLevelOne = findViewById(R.id.radioButtonLevelOne);
-        radioButtonLevelTwo = findViewById(R.id.radioButtonLevelTwo);
-        radioButtonLevelThree = findViewById(R.id.radioButtonLevelThree);
-        radioButtonLevelOne.setChecked(false);
-        radioButtonLevelTwo.setChecked(false);
-        radioButtonLevelThree.setChecked(false);
+        btn_chkscore = (Button)findViewById(R.id.btn_chkscore);
+        btn_chkscore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        // Define Time views
-        radioButton30 = findViewById(R.id.radioButton30);
-        radioButton60 = findViewById(R.id.radioButton60);
-        radioButton90 = findViewById(R.id.radioButton90);
-        radioButton30.setChecked(false);
-        radioButton60.setChecked(false);
-        radioButton90.setChecked(false);
-
-        // Define Download views
-        progressBarDownload = findViewById(R.id.progressBarDownload);
-        progressBarDownload.setMax(100);
-
-        // Define Update and Starting buttons
-        Button buttonUpdate = findViewById(R.id.buttonUpdate);
-        buttonStartQuiz = findViewById(R.id.buttonStartQuiz);
-        buttonUpdate.setEnabled(true);
-        buttonStartQuiz.setEnabled(false);
-        downloadTask = null; // Always initialize task to null
-    }
-
-    private DownloadTask downloadTask;
-
-    // Define Download methods
-    private DownloadListener downloadListener = new DownloadListener() {
-        @Override
-        public void onProgress(int progress) {
-            progressBarDownload.setProgress(progress);
-        }
-
-        @Override
-        public void onSuccess() {
-            downloadTask = null;
-            progressBarDownload.setProgress(progressBarDownload.getMax());
-            buttonStartQuiz.setEnabled(true); // Enable Start button when download is successful
-        }
-
-        @Override
-        public void onFailed() {
-            downloadTask = null;
-            //when download failed, close the foreground notification and create a new one about the failure
-            Toast.makeText(getApplicationContext(), "Download Failed", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onPaused() {
-            downloadTask = null;
-            Toast.makeText(getApplicationContext(), "Paused", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onCanceled() {
-            downloadTask = null;
-            Toast.makeText(getApplicationContext(), "Canceled", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    public void onButtonUpdate(View view) {
-        if(downloadTask == null) {
-            // Import data from internet
-            String jsonUrl = "https://api.jsonbin.io/b/5e8f60bb172eb6438960f731";
-            downloadTask = new DownloadTask(downloadListener, this);
-            downloadTask.execute(jsonUrl);
-        }
-    }
-
-    // Start QuizActivity with user settings/choices
-    public void onButtonStartQuiz(View view) {
-        if(radioButtonLevelOne.isChecked()) level = 1;
-        if(radioButtonLevelTwo.isChecked()) level = 2;
-        if(radioButtonLevelThree.isChecked()) level = 3;
-
-        if(radioButton30.isChecked()) seconds = 30;
-        if(radioButton60.isChecked()) seconds = 60;
-        if(radioButton90.isChecked()) seconds = 90;
-        
-        Intent intent = new Intent(this, QuizActivity.class);
-        intent.putExtra("level", level);
-        intent.putExtra("seconds", seconds);
-        startActivity(intent);
     }
 }
